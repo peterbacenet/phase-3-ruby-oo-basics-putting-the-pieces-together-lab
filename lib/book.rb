4b1a646fdb0dd5742741defd1b6f4227c5aa69fe
Peter Bacenet <peterbacenet@icloud.com>
2021-09-27 11:00:07 -0400

    commit tree e02b95afb6498324c8decad69eac8fc4e27ba5a2

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+require 'pry'
+class Book
+    attr_accessor :author, :page_count, :genre #properties
+    attr_reader :title
 
+    def initialize(title, author, page_count, genre)
+        @title = title
+    end
+    def turn_page
+        #add 1 to page count
+        @page_count + 1
+    end
+end
+binding.pry
 
